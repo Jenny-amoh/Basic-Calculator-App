dd58f1a9c35639d7b69fc84f3dd67d395734bd12
Jenny-amoh <jenniferamoh214@gmail.com>
2025-08-15 17:02:27 +0100

    html and css done

---
+++ script.js
@@ -0,0 +1,3 @@
+const result = document.getElementsByClassName('.resultField');
+const operator = document.getElementsByClassName('.button.operator')
+const equals = document.getElementsByClassName('.')
